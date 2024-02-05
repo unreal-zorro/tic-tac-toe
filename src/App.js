052170eeed459e268cc4a34b872e2b21061dd1ac
unreal-zorro <unrealzorro@gmail.com>
2024-02-05 20:52:26 +0300

    add 2 cycles in board

--- src/App.js
+++ src/App.js
@@ -38,21 +38,23 @@ function Board({ xIsNext, squares, onPlay }) {
   return (
     <>
       <div className="status">{status}</div>
-      <div className="board-row">
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-      </div>
+      {new Array(3).fill(null).map((_, i) => {
+        return (
+          <div
+            key={i}
+            className="board-row"
+          >
+            {new Array(3).fill(null).map((_, j) => {
+              return (
+                <Square
+                  key={i * 3 + j}
+                  value={squares[i * 3 + j]} onSquareClick={() => handleClick(i * 3 + j)}
+                />
+              );
+            })}
+          </div>
+        );
+      })}
     </>
   );
 }
